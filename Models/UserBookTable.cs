526cdc7962f6ff9f7f02add87145471a72abfee6
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 13:16:51 -0300

    :sparkles: feat: Add UserBookTable model

---
+++ Models/UserBookTable.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Models
+{
+    [Table("UserBook_Table")]
+    public class UserBookTable
+    {
+        public int IdUser { get; set; }
+        public int IdBook { get; set; }
+        public DateTime GetDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+
+    }
+}
