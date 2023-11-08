0ba72e25732291932234fd38b12403e398676fc8
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 13:06:19 -0300

    :sparkles: feat: Add WriterTable Model

---
+++ Models/WriterTable.cs
@@ -0,0 +1,22 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Models
+{
+    [Table("Writer_Table")]
+    public class WriterTable
+    {
+        public WriterTable()
+            => Books = new List<BookTable>();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Write(false)]
+        public List<BookTable> Books { get; set; }
+    }
+}
