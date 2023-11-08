c4ba79282a9c4a029074e67b28268bb08627d8b8
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 12:46:10 -0300

    :sparkles: feat: Add PhoneTable Model

---
+++ Models/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Models
+{
+    [Table("Phone_Table")]
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        [Column("Phone")]
+        public string phone { get; set; }
+   
+    }
+}
