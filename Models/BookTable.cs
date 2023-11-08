fd31b22fa0d48658e20a822e0ab478cc4675d96e
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 13:10:25 -0300

    :sparkles: feat: Add Category List in BookModel

--- Models/BookTable.cs
+++ Models/BookTable.cs
@@ -10,11 +10,18 @@ namespace CapiLibrary.Models
     [Table("Book_Table")]
     public class BookTable
     {
+        public BookTable()
+            => Categories = new List<CategoryTable>();
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Synopsis { get; set; }
         public int Pages { get; set; }
         public string GeneralAud { get; set; }
+
+
+        [Write(false)]
+        public List<CategoryTable> Categories { get; set; }
 
     }
 }
