0ac848ff31e508f466a82a81db3c8b8860cf7035
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 13:08:02 -0300

    :sparkles: feat: Add CategoryTable Model

---
+++ Models/CategoryTable.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Models
+{
+    [Table("Category_Table")]
+    public class CategoryTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
