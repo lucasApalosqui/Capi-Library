03ba0eb14b950ee0e242796ececded64c261789c
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 16:29:18 -0300

    :sparkles: feat: Add Utilitie Menu Methods
    Skip Line with dott, Skip Line with a number of lines param and Generate Option select menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using CapiLibrary;
+using CapiLibrary.Utilities;
 using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING = "Server=DESKTOP-BVKU5HC;Database=CapiLibrary;Trusted_Connection=True;TrustServerCertificate=True";
 Database.Connection = new SqlConnection(CONNECTION_STRING);
 Database.Connection.Open();
-
 
 Database.Connection.Close();
 
---
+++ Utilities/MenuWrite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Utilities
+{
+    public static class MenuWrite
+    {
+        public static void Dotted()
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine();
+        }
+
+        public static void SkipLine(int line)
+        {
+            for (int i = 0; i < line; i++)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        public static void OptionGen(List<string> options)
+        {
+            Console.WriteLine("O que deseja fazer?");
+            SkipLine(1);
+            var num = 0;
+            foreach (string option in options)
+            {
+                num++;
+                Console.WriteLine($"{num} - {option}");
+            }
+        }
+    }
+}
