c168311134c2156e834e316bcf685f6dce3d83b9
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 14:06:48 -0300

    :sparkles: feat: Add Database Connection

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using CapiLibrary;
+using Microsoft.Data.SqlClient;
+
+const string CONNECTION_STRING = "Server=DESKTOP-BVKU5HC;Database=CapiLibrary;Trusted_Connection=True;TrustServerCertificate=True";
+Database.Connection = new SqlConnection(CONNECTION_STRING);
+Database.Connection.Open();
+
+
+Database.Connection.Close();
+
