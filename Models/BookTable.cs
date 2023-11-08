4b55931990832b9b863e996f649025c79443a3e9
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-08 13:02:44 -0300

    :sparkles: feat: Add BookTable Model

---
+++ Models/BookTable.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapiLibrary.Models
+{
+    [Table("Book_Table")]
+    public class BookTable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public int Pages { get; set; }
+        public string GeneralAud { get; set; }
+
+    }
+}
