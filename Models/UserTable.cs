2d730db1eb92e34910371f94e2761d1b6a856d01
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-11-09 16:06:34 -0300

    :recycle: refactor: alter FirstName

--- Models/UserTable.cs
+++ Models/UserTable.cs
@@ -12,7 +12,7 @@ namespace CapiLibrary.Models
     {
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Cpf { get; set; }
         public string Email { get; set; }
--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING = "Server=DESKTOP-BVKU5HC;Database=CapiLibrary;Trusted_Connection=True;TrustServerCertificate=True";
 Database.Connection = new SqlConnection(CONNECTION_STRING);
-//Database.Connection.Open();
+Database.Connection.Open();
 
 MainMenuScreen.Load();
 
-//Database.Connection.Close();
+Database.Connection.Close();
 
